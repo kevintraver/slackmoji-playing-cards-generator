26d01db587fa363b022edb1124e1148506c07c6d
Ruston Reformado <rustonreformado@gmail.com>
2018-03-13 19:46:55 -0600

    added aliases

--- yamlGenerator.js
+++ yamlGenerator.js
@@ -3,7 +3,7 @@ const yaml = require('js-yaml');
 const fs = require('fs');
 
 let suits = ["spades", "diamonds", "clubs", "hearts"];
-let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"];
 let baseCardUrl = process.env.BASE_CARD_URL;
 
 function buildDeck(){
@@ -12,20 +12,49 @@ function buildDeck(){
 	for(let i = 0; i < suits.length; i++) {
 		for(let x = 0; x < values.length; x++) {
 			let card = {
-			  value: values[x], 
+			  value: values[x].toLowerCase(), 
 			  suit: suits[i]
 			};
             card.name = card.value + card.suit[0].toUpperCase();
-            card.url = `${baseCardUrl}${card.name}.png`;
+            card.url = `${baseCardUrl}${getCardSlug(card)}.png`;
+            card.aliases = getCardAliases(card);
 			deck.push(card);
 		}
 	}
     return deck;
 }
 
+function getCardSlug(card){
+    if(Number.isInteger(card.value)){
+        return card.value + card.suit[0];
+    } else {
+        return card.value[0] + card.suit[0];
+    }
+}
+
+function getCardAliases(card) {
+    let suit = card.suit;
+    let value = card.value;
+    let aliases = [];
+    aliases.push(`${suit[0]}${value[0]}`);
+    aliases.push(`${value[0]}${suit[0]}`);
+
+    aliases.push(`${value[0]}${suit}`);
+    aliases.push(`${suit[0]}${value}`);
+
+    aliases.push(`${value}${suit[0]}`);
+    aliases.push(`${suit}${value[0]}`);
+
+    aliases.push(`${suit}${value}`);
+    aliases.push(`${value}${suit}`);
+
+    return aliases;
+}
+
 let yamlDeck = buildDeck().map( (card) => {
     return {
-        name: card.name,
+        name: card.name.toLowerCase(),
+        aliases: card.aliases,
         src: card.url
     }
 })
